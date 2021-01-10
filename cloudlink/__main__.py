045c6842d77f89d55251d61b7233958a80473e04
cSDes1gn <christian911@sympatio.ca>
2021-01-10 07:31:24 +0000

    integrate serial interface on rpi for arduino

--- cloudlink/__main__.py
+++ cloudlink/__main__.py
@@ -2,6 +2,7 @@
 import time
 import os
 import pyrebase
+import serial
 from datetime import datetime
 from cloudlink.rtd.rtd import RTDLink
 
@@ -16,15 +17,36 @@ if __name__ == "__main__":
     )
 
     # telemetry reporting loop
-    while True:
-        temp = float(input("T: "))
-        humidity = float(input("H: "))
-        lux = float(input("L: "))
-        firebase.publish(
-            {
-                "T": temp,
-                "H": humidity,
-                "L": lux,
-                "I": datetime.isoformat(datetime.utcnow())
-            }
+    try:
+        serial_connection = serial.Serial(
+            port='/dev/ttyUSB0',
+            baudrate=9600,
+            timeout=5.0
         )
+    except (serial.SerialException, FileNotFoundError) as exc:
+        print("Serial connection failed")
+        exit(1)
+
+    while True:
+        try:
+            line = serial_connection.readline()
+        except serial.SerialException as exc:
+            print("Failed to read sensorframe")
+        else:
+            humidity = 0.0
+            lux = 0.0
+            sensorframe = line.decode()
+            print("Sensorframe: {}".format(sensorframe))
+            for value in sensorframe.split('|'):
+                if value.split('&')[0] == 'SM':
+                    humidity = int(value.split('&')[1])
+                elif value.split('&')[0] == 'LX':
+                    lux = int(value.split('&')[1])
+            firebase.publish(
+                {
+                    "T": 0.0,
+                    "H": humidity,
+                    "L": lux,
+                    "I": datetime.isoformat(datetime.utcnow())
+                }
+            )
