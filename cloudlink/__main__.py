b3bfa5a4b70400bdf7ea6eebaf9013dbca7891cc
cSDes1gn <christian911@sympatico.ca>
2021-01-26 23:04:06 -0500

    create mock for serial sf

--- cloudlink/__main__.py
+++ cloudlink/__main__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import time
 import os
-import pyrebase
 import serial
 from datetime import datetime
 from cloudlink.rtd.rtd import RTDLink
+import random
 
 
 if __name__ == "__main__":
@@ -17,41 +17,58 @@ if __name__ == "__main__":
     )
 
     # telemetry reporting loop
-    try:
-        serial_connection = serial.Serial(
-            port='/dev/ttyUSB0',
-            baudrate=9600,
-            timeout=30.0
-        )
-    except (serial.SerialException, FileNotFoundError) as exc:
-        print("Serial connection failed")
-        exit(1)
-
-    while True:
+    if os.environ['CL_DEBUG'] != "true":
         try:
-            line = serial_connection.readline()
-        except serial.SerialException as exc:
-            print("Failed to read sensorframe")
-        else:
-            humidity = 0.0
-            lux = 0.0
-            cpu_temp = 0.0
-            sensorframe = line.decode()
-            print("Sensorframe: {}".format(sensorframe))
-            with open( "/sys/class/thermal/thermal_zone0/temp" ) as tempFile:
-                cpu_temp = tempFile.read()
-                tempFile.close()
-            temp = round(float(cpu_temp)/1000, 2)
-            for value in sensorframe.split('|'):
-                if value.split('&')[0] == 'SM':
-                    humidity = int(value.split('&')[1])
-                elif value.split('&')[0] == 'LX':
-                    lux = int(value.split('&')[1])
-            firebase.publish(
-                {
-                    "T": temp,
-                    "H": humidity,
-                    "L": lux,
-                    "I": datetime.isoformat(datetime.utcnow())
-                }
+            serial_connection = serial.Serial(
+                port='/dev/ttyUSB0',
+                baudrate=9600,
+                timeout=30.0
             )
+        except (serial.SerialException, FileNotFoundError) as exc:
+            print("Serial connection failed")
+            exit(1)
+
+        while True:
+            try:
+                line = serial_connection.readline()
+            except serial.SerialException as exc:
+                print("Failed to read sensorframe")
+            else:
+                humidity = 0.0
+                lux = 0.0
+                cpu_temp = 0.0
+                temp = round(float(cpu_temp)/1000, 2)
+                sensorframe = line.decode()
+                print("Sensorframe: {}".format(sensorframe))
+                for value in sensorframe.split('|'):
+                    if value.split('&')[0] == 'SM':
+                        humidity = int(value.split('&')[1])
+                    elif value.split('&')[0] == 'LX':
+                        lux = int(value.split('&')[1])
+                firebase.publish(
+                    {
+                        "T": temp,
+                        "H": humidity,
+                        "L": lux,
+                        "I": datetime.isoformat(datetime.utcnow())
+                    }
+                )
+    else:
+        # debug mode mocking serial sensorframe
+        while True:
+            time.sleep(10)
+            sh = random.randint(0,100)
+            rh = random.randint(0,100)
+            lux = random.randint(0,10000)
+            pa = random.randint(90,110)
+            temp = random.randint(15,35)
+            payload = {
+                "TP": temp,
+                "RH": rh,
+                "PA": pa,
+                "LX": lux,
+                "SM": sh,
+                "I": datetime.isoformat(datetime.utcnow())
+            }
+            firebase.publish(payload)
+            print("Payload: {}".format(payload))
